0e5b91430d6625e60e049962a7f1d947d6bf6a58
sench32 <you@example.com>
2021-03-31 07:55:47 +0300

    add class MySeperScrol()

---
+++ lib/my_sweper.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class My_Sweper extends StatefulWidget {
+  My_Sweper({Key key}) : super(key: key);
+
+  @override
+  _My_SweperState createState() => _My_SweperState();
+}
+
+class _My_SweperState extends State<My_Sweper> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
